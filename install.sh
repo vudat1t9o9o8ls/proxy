d53f642c26b1774b9dbaf7a8f6aa9626d1c0e500
vudat1t9o9o8ls <105526820+vudat1t9o9o8ls@users.noreply.github.com>
2022-05-15 09:36:43 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,19 +26,16 @@ install_3proxy() {
 }
 
 gen_3proxy() {
-    cat <<EOF
-daemon
+    daemon
 maxconn 1000
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 flush
-auth none
-
-
-
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+auth strong
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" " "}' ${WORKDATA})
+$(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
